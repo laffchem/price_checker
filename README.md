76f853a00f11c7da4b5e27587114dca1963a4c3a
John Laffey <laffeychem@gmail.com>
2022-05-04 15:46:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# price_checker
+
+This is a simple price checking app that utilizes webscraping to look at specific
+products on amazon. (You have to change the url in the code to whatever you want.)
+
+You need to have headers in for bs4 to work. So just go to myhttpheader.com and paste the language and user-agent 
+in the appropriate variables.
+
+Go to camelcamelcamel.com to compare the lowest price of your product and then insert that into your if statement.
+
+You'll also have to utilize pushover (or your own method) to send the alert to yourself.
+
+I'm running this on a headless pi that I use to automate daily notifications and tasks so I can be lazy.
+
+Enjoy!
---
+++ main.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+
+USER_KEY = os.environ.get('user_key')
+APP_KEY = os.environ.get('app_key')
+URL = "https://www.amazon.com/Logitech-Advanced-Wireless-Illuminated-Keyboard/dp/B07S92QBCJ"
+
+#Headers to scrape Amazon.
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; CrOS aarch64 14526.89.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.133 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+}
+#Scrape MX Keys keyboard for lowest price
+response = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+mx_price = soup.find(name="span", class_="a-offscreen")
+
+price = mx_price.getText().replace('$', '')
+
+#Check if price is lower than camelcamelcamel.com lowest price
+if float(price) <= 100:
+    message = "The price is right! Buy it now!"
+else:
+    message = "This shit is too expensive!"
+
+#JSON Data for pushover app.
+price_checker = {
+    "token": APP_KEY,
+    "user": USER_KEY,
+    "message": message,
+    "url": URL,
+    "url_title": "MX Keys Keyboard",
+}
+
+response = requests.post("https://api.pushover.net/1/messages.json", json=price_checker)
+print(response)
